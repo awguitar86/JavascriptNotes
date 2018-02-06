50e1550fc323ec7cc6508c9fb24e32ec5bc641e9
Austin Wright <awguitar86@gmail.com>
2018-02-05 19:44:31 -0700

    added a note for a function to put an integer in descending order

--- VSCode/JS-Arrays.js
+++ VSCode/JS-Arrays.js
@@ -359,3 +359,21 @@ const both = (arr1, arr2) => {
   return newArr;
 }
 both(arr1, arr2);
+
+
+/* DESCENDING ORDER */
+//Your task is to make a function that can take any non-negative integer as a argument and return it with its digits in descending order. 
+//Essentially, rearrange the digits to create the highest possible number.
+var number = 1234567890;
+
+function descendingOrder(n){
+  var arr = n.toString().split('');
+  var newArr = [];
+  for(var i = 0; i < arr.length; i++){
+    newArr.push(parseInt(arr[i]));
+  }
+  var sorted = newArr.sort((a,b) => b-a);
+  var sorted2 = sorted.join('');
+  return parseInt(sorted2);
+}
+descendingOrder(number);
